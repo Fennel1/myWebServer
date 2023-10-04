ac75beaf0140eb5df6ed6f2290805453997cf85e
Fennel1 <627235787@qq.com>
2023-10-04 20:57:24 +0800

    webserver

---
+++ webserver/webserver.h
@@ -0,0 +1,64 @@
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+#include "../threadpool/threadpool.h"
+#include "../http/http_conn.h"
+
+const int MAX_FD = 65536;           //最大文件描述符
+const int MAX_EVENT_NUMBER = 10000; //最大事件数
+const int TIMESLOT = 5;             //最小超时单位
+
+class WebServer{
+public:
+    WebServer();
+    ~WebServer();
+
+    void init(int port , string user, string passWord, string databaseName,
+              int log_write , int linger, int et, int sql_num,
+              int thread_num, int close_log, int actor_model);
+    void thread_pool();
+    void sql_pool();
+    void log_write();
+    void et();
+    void eventListen();
+    void eventLoop();
+    void timer(int connfd, struct sockaddr_in client_address);
+    void adjust_timer(util_timer *timer);
+    void deal_timer(util_timer *timer, int socketfd);
+    bool dealclientdata();
+    bool dealwithsignal(bool &timeout, bool &stop_server);
+    void dealwithread(int socketfd);
+    void dealwithwrite(int socketfd);
+
+    int port_;
+    char *root_;
+    int log_write_;
+    int close_log_;
+    int actorModel_;
+
+    int pipefd[2];
+    int epollfd_;
+    http_conn *users;
+
+    connection_pool *connPool_;
+    std::string user_;
+    std::string passwrod_;
+    std::string databaseName_;
+    int sqlNUm_;
+
+    threadpool<http_conn> pool_;
+    int threadNum_;
+
+    epoll_event events[MAX_EVENT_NUMBER];
+
+    int listenfd_;
+    int linger_;
+    int et_;
+    int listenfdMode_;
+    int connfdMode_;
+
+    client_data *users_timer;
+    Utils utils;
+};
+
+#endif
