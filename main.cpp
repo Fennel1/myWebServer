da9b6fcca2594835d87ad5024acd882e514e0ee8
Fennel1 <627235787@qq.com>
2023-10-05 22:52:47 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "config/config.h"
+#include "webserver/webserver.h"
+
+int main(int argc, char *argv[]){
+    std::string user = "root";
+    std::string passwd = "root";
+    std::string databasename = "qgydb";
+
+    Config config;
+    config.parse_arg(argc, argv);
+
+    WebServer server;
+    server.init(config.port_, user, passwd, databasename, config.logwrite_, 
+                config.linger_, config.et_,  config.sqlNum_,  config.threadNum_, 
+                config.closeLog_, config.actorModel_);
+    
+    server.log_write();
+    server.sql_pool();
+    server.thread_pool();
+    server.et();
+    server.eventListen();
+    server.eventLoop();
+
+    return 0;
+}
